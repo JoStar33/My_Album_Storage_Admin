1d03ecc7e3a28b3f8237a923c6125694d299c0d3
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-01-16 18:17:00 +0900

    로그인 페이지에 토큰 유무 설정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components'
+import Cookies from 'universal-cookie';
 import LoginForm from '../components/forms/commonForms/LoginForm';
+import { Navigate } from "react-router-dom";
+import { getToken } from "../apis/tokens/token";
 
 const LoginPage: React.FC = () => {
-  return (
+  const cookies = new Cookies();
+  const isAuthorized = getToken();
+  useEffect(() => {
+    if(!isAuthorized) {
+      cookies.remove('connect.sid');
+    }
+  });
+  return !isAuthorized || isAuthorized === null ? (
     <LoginPageContainer>
       <LoginForm></LoginForm>
     </LoginPageContainer>
+  ) : (
+    <Navigate to="/" />
   );
 };
 
